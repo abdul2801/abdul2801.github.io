3b1ee29217ab065bd307b90e770b2379c32bb1c1
afdul <afdul100ahmad@gmail.com>
2026-01-24 15:04:31 +0530

    updates

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,77 +2,78 @@
 
 const CONFIG = {
   github: {
-    username: 'abdul2801', // Your GitHub org/user name. (This is the only required config)
+    username: "abdul2801", // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '',
+  base: "",
   projects: {
-    
     github: {
       display: true, // Display GitHub projects?
-      header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      header: "Github Projects",
+      mode: "automatic", // Mode can be: 'automatic' or 'manual'
       automatic: {
-        sortBy: 'updated', // Sort projects by 'stars' or 'updated'
+        sortBy: "updated", // Sort projects by 'stars' or 'updated'
         limit: 4, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ['abdul2801/cpp'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ["abdul2801/cpp"], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ["arifszn/gitprofile", "arifszn/pandora"], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
-    
+
     external: {
-      header: 'My Projects',
+      header: "My Projects",
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Packet Sniffer',
+          title: "Packet Sniffer",
           description:
-            'Developed a packet sniffer in Rust using the C `Pcap` library for capturing raw network packets. Integrated a terminal user interface (TUI) with `Rattui` for real-time interactive packet data inspection.\n\nTech Stack: Rust, C, Pcap, Rattui.',
-          imageUrl: 'https://images.squarespace-cdn.com/content/v1/569805af1f4039853c5804f1/1526586326433-ER7GN1K932RALLWJENEC/SentryWire-Packet-Capture-Appliance-230-1.png?format=500w', // Replace with real image if available
-          link: 'https://github.com/abdul2801/sniffer',
+            "Developed a packet sniffer in Rust using the C `Pcap` library for capturing raw network packets. Integrated a terminal user interface (TUI) with `Rattui` for real-time interactive packet data inspection.\n\nTech Stack: Rust, C, Pcap, Rattui.",
+          imageUrl:
+            "https://images.squarespace-cdn.com/content/v1/569805af1f4039853c5804f1/1526586326433-ER7GN1K932RALLWJENEC/SentryWire-Packet-Capture-Appliance-230-1.png?format=500w", // Replace with real image if available
+          link: "https://github.com/abdul2801/sniffer",
         },
         {
-          title: 'QueueShopper - Distributed Order Processing System',
+          title: "QueueShopper - Distributed Order Processing System",
           description:
-            'Developed a distributed order processing system using Apache Kafka for message queuing and MySQL for persistence. Implemented producer-consumer architecture to ensure high throughput and fault tolerance in handling e-commerce transactions.\n\nTech Stack: Kafka, Node.js, MySQL.',
-          imageUrl: 'https://raw.githubusercontent.com/abdul2801/queueShopper/main/screenshot.png', // Replace with real image if available
-          link: 'https://github.com/abdul2801/queueShopper',
+            "Developed a distributed order processing system using Apache Kafka for message queuing and MySQL for persistence. Implemented producer-consumer architecture to ensure high throughput and fault tolerance in handling e-commerce transactions.\n\nTech Stack: Kafka, Node.js, MySQL.",
+          imageUrl:
+            "https://raw.githubusercontent.com/abdul2801/queueShopper/main/screenshot.png", // Replace with real image if available
+          link: "https://github.com/abdul2801/queueShopper",
         },
         {
-          title: 'Collaborative Code Editor',
+          title: "Collaborative Code Editor",
           description:
-            'Developed a real-time collaborative code editor with VSCode-like features. Utilized CRDT (Conflict-Free Replicated Data Types) with Yjs for distributed data synchronization and WebSockets for low-latency communication.\n\nTech Stack: React, Yjs, WebSockets.',
-          imageUrl: 'https://raw.githubusercontent.com/abdul2801/Collaborative-code-editor/main/image.png', // Replace with real image if available
-          link: 'https://github.com/abdul2801/Collaborative-code-editor',
+            "Developed a real-time collaborative code editor with VSCode-like features. Utilized CRDT (Conflict-Free Replicated Data Types) with Yjs for distributed data synchronization and WebSockets for low-latency communication.\n\nTech Stack: React, Yjs, WebSockets.",
+          imageUrl:
+            "https://raw.githubusercontent.com/abdul2801/Collaborative-code-editor/main/image.png", // Replace with real image if available
+          link: "https://github.com/abdul2801/Collaborative-code-editor",
         },
         {
-          title: 'Capture The Flag (CTF) Platform',
+          title: "Capture The Flag (CTF) Platform",
           description:
-            'Built an interactive Capture the Flag (CTF) platform with a React frontend and Node.js backend. Integrated an admin interface for creating and managing challenges to enhance platform flexibility.\n\nTech Stack: React, Node.js.',
-          imageUrl: 'https://avatars.githubusercontent.com/u/129326094?v=4', // Assuming same image applies
-          link: 'https://ctf.iiitkottayam.ac.in/',
-        }
-
+            "Built an interactive Capture the Flag (CTF) platform with a React frontend and Node.js backend. Integrated an admin interface for creating and managing challenges to enhance platform flexibility.\n\nTech Stack: React, Node.js.",
+          imageUrl: "https://avatars.githubusercontent.com/u/129326094?v=4", // Assuming same image applies
+          link: "https://ctf.iiitkottayam.ac.in/",
+        },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Abdul Ahmad',
-    description: '',
-    imageURL: '',
+    title: "Portfolio of Abdul Ahmad",
+    description: "",
+    imageURL: "",
   },
   social: {
-    linkedin: 'abdul-ahmad-2398a1272',
+    linkedin: "abdul-ahmad-2398a1272",
     // twitter: 'arif_szn',
     // mastodon: 'arifszn@mastodon.social',
     // researchGate: '',
@@ -90,78 +91,82 @@ const CONFIG = {
     // skype: '',
     // telegram: '',
     // website: 'https://www.arifszn.com',
-    leetcode : 'afdul',
-    phone: '+91 9344841926',
-    email: 'abdul100rafey@gmail.com',
+    leetcode: "afdul",
+    phone: "+91 9344841926",
+    email: "abdul100rafey@gmail.com",
   },
   resume: {
-    fileUrl:
-      '/resume_new.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: "/resume_new.pdf", // Empty fileUrl will hide the `Download Resume` button.
   },
   // 20 only
   skills: [
-    'Rust',
-    'Java',
-    'C',
-    'C++',
-    'JavaScript',
-    'TypeScript',
-    'React',
-    'Node.js',
-    'Express.js',
-    'Python',
-    'PostgreSQL',
-    'MySQL',
-    'DB2',
-    'Docker',
-    'Kubernetes',
-    'AWS (EC2, S3, Lambda)',
-    'Nginx',
-    'Celery',
-    'Redis',
-    'Linux',
-    'Git',
-    'GitHub',
-    'GitLab'
+    "Rust",
+    "Java",
+    "C",
+    "C++",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Node.js",
+    "Express.js",
+    "Python",
+    "PostgreSQL",
+    "MySQL",
+    "DB2",
+    "Docker",
+    "Kubernetes",
+    "AWS (EC2, S3, Lambda)",
+    "Nginx",
+    "Celery",
+    "Redis",
+    "Linux",
+    "Git",
+    "GitHub",
+    "GitLab",
   ],
 
   experiences: [
     {
-      company: 'Soundverse',
-      position: 'Devops And Backend Intern',
-      from: 'April 2025',
-      to: 'July 2025',
-      companyLink: 'https://www.soundverse.ai/',
-      
+      company: "Siemens",
+      position: "Research Intern",
+      from: "December 2025",
+      to: "Present",
+      companyLink: "https://www.siemens.com/in/en.html",
     },
     {
-      company: 'IIIT Hyderabad',
-      position: 'Research Intern',
-      from: 'June 2025',
-      to: 'July 2025',
-      companyLink: 'https://www.iiit.ac.in/',
-      
+      company: "Soundverse",
+      position: "Devops And Backend Intern",
+      from: "March 2025",
+      to: "November 2025",
+      companyLink: "https://www.soundverse.ai/",
     },
     {
-      company: 'BuildFast',
-      position: 'ReactJs Intern',
-      from: 'September 2023',
-      to: 'November 2023',
-      companyLink: 'https://www.buildfast.co.in/',
+      company: "IIIT Hyderabad",
+      position: "Research Intern",
+      from: "June 2025",
+      to: "July 2025",
+      companyLink: "https://www.iiit.ac.in/",
     },
     {
-      company: 'Google Cloud',
-      position: 'Study Jam Facilitator',
-      from: 'November 2023',
-      to: 'January 2024',
-      companyLink: 'https://cloud.google.com/',
+      company: "BuildFast",
+      position: "ReactJs Intern",
+      from: "September 2023",
+      to: "November 2023",
+      companyLink: "https://www.buildfast.co.in/",
     },
     {
-      company: 'OpenSource Club ',
-      position: 'Sublead',
-      from: 'December 2023',
-      to: 'Present',
-      companyLink: '',
+      company: "Google Cloud",
+      position: "Study Jam Facilitator",
+      from: "November 2023",
+      to: "January 2024",
+      companyLink: "https://cloud.google.com/",
+    },
+    {
+      company: "OpenSource Club ",
+      position: "Sublead",
+      from: "December 2023",
+      to: "July 2024",
+      companyLink: "",
     },
   ],
   // certifications: [
@@ -174,29 +179,29 @@ const CONFIG = {
   // ],
   educations: [
     {
-      institution: 'Indian Institute of Information Technology, Kottayam',
-      degree: 'Computer Science and Engineering',
-      from: '2022',
-      to: 'Present',
+      institution: "Indian Institute of Information Technology, Kottayam",
+      degree: "Computer Science and Engineering",
+      from: "2022",
+      to: "Present",
     },
     {
-      institution: 'Chennai Public School, Chennai',
-      degree: 'Higher Secondary School',
-      from: '2012',
-      to: '2019',
+      institution: "Chennai Public School, Chennai",
+      degree: "Higher Secondary School",
+      from: "2012",
+      to: "2019",
     },
   ],
   publications: [
     {
-      title: 'Study of the Representational Power of Weighted Randomised Univariate Regression Tree',
-      conferenceName: '',
+      title:
+        "Study of the Representational Power of Weighted Randomised Univariate Regression Tree",
+      conferenceName: "",
       // journalName: 'Journal Name',
-      authors: 'Amir Ahmad, Abdul Rafey, Ajay Kumar',
-      link: 'https://www.worldscientific.com/doi/10.1142/S021964922450045X',
+      authors: "Amir Ahmad, Abdul Rafey, Ajay Kumar",
+      link: "https://www.worldscientific.com/doi/10.1142/S021964922450045X",
       description:
-      "Developed a theoretical framework for studying the representational power of infinite-sized weighted ensembles of randomized regression trees. Demonstrated that weighted ensembles can outperform unweighted ones for specific datasets, depending on the weighting scheme. Provided accurate predictions for experimental results in certain cases, enhancing the understanding of regression tree ensembles' properties"
+        "Developed a theoretical framework for studying the representational power of infinite-sized weighted ensembles of randomized regression trees. Demonstrated that weighted ensembles can outperform unweighted ones for specific datasets, depending on the weighting scheme. Provided accurate predictions for experimental results in certain cases, enhancing the understanding of regression tree ensembles' properties",
     },
-  
   ],
   // Display articles from your medium or dev account. (Optional)
   // blog: {
@@ -205,15 +210,15 @@ const CONFIG = {
   //   limit: 2, // How many articles to display. Max is 10.
   // },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: "", // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
+    id: "",
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: "dracula",
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -228,7 +233,7 @@ const CONFIG = {
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
-      'light',
+      "light",
       // 'dark',
       // 'cupcake',
       // 'bumblebee',
@@ -248,7 +253,7 @@ const CONFIG = {
       // 'wireframe',
       // 'black',
       // 'luxury',
-      'dracula',
+      "dracula",
       // 'cmyk',
       // 'autumn',
       // 'business',
@@ -265,18 +270,18 @@ const CONFIG = {
 
     // Custom theme, applied to `procyon` theme
     customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
+      primary: "#fc055b",
+      secondary: "#219aaf",
+      accent: "#e8d03a",
+      neutral: "#2A2730",
+      "base-100": "#E3E3ED",
+      "--rounded-box": "3rem",
+      "--rounded-btn": "3rem",
     },
   },
 
   // Optional Footer. Supports plain text or HTML.
-  // footer: `Made with <a 
+  // footer: `Made with <a
   //     class="text-primary" href="https://github.com/arifszn/gitprofile"
   //     target="_blank"
   //     rel="noreferrer"
